ee1b072aeae159e63a2fa130c0c9179bc8bee00a
SamYu <3832817+syslink@users.noreply.github.com>
2020-07-15 19:10:31 +0800

    update notfound info

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -15,10 +15,10 @@ export default function NotFound() {
           />
           <div className="prompt">
             <h3 className={styles.title}>
-              抱歉，你访问的页面不存在
+              此功能正在开发中，敬请期待...
             </h3>
             <p className={styles.description}>
-              您要找的页面没有找到，请返回
+              请返回
               <Link to="/">首页</Link>
               继续浏览
             </p>
